79242f14d34dbbe34a8221621a958ba62fdb8c1c
ari <aniknia.dev@gmail.com>
2023-01-25 00:41:54 -0800

    feat: add umami tracker

--- components/general/meta.tsx
+++ components/general/meta.tsx
@@ -10,6 +10,12 @@ export default function Meta() {
           content="An easy way to make secure memorable passwords."
         />
         <link rel="icon" type="image/png" href="favicon.png" />
+        <script
+          async
+          defer
+          data-website-id="3903ed67-4625-4a88-8cf2-36e7b2e4cb80"
+          src="https://umami.niknia.dev/umami.js"
+        ></script>
       </Head>
     </>
   );
