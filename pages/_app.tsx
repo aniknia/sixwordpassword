824de65e9f8c73b63e54103ea0d75377c9abef3f
ari <aniknia.dev@gmail.com>
2023-12-03 22:15:55 -0800

    build: update dependencies

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
+'use client'
+
 import type { AppProps } from "next/app";
-import { chakra } from "@chakra-ui/react";
 import { ChakraProvider } from "@chakra-ui/react";
 import Layout from "../components/layout/layout";
 import theme from "../components/general/theme";
